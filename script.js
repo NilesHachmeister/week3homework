051e0a01fd40eb1c7eacb086e34769f5439d2ae7
Niles <Nilesquackmeister@gmail.com>
2022-01-07 13:29:26 -0700

    made it so that it creates an array based on the specifications and turns that into a string'

--- script.js
+++ script.js
@@ -18,17 +18,24 @@ var passCriteria = {
   special: true,
 }
 
+const lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+", "[", "{", "]", "}", "|", ";", ":", "'", ",", "<", ".", ">", "/", "?", "`", "~"]
+var finalArray = [];
+var passwordArray = [];
+// make arrays out of everything, and then concanonate them based on what the user wants, then random number genorate between the max and min, find the index of that, and spit it out.
 
-var inputLength;
-function getNumber() {
+
+function inputLength() {
   inputLength = prompt("Length of password?");
-  while (inputLength < 7 || inputLength > 129) {
+
+  while (!(inputLength > 7 && inputLength < 129)) {
     inputLength = prompt("Incorrect length, please try again");
   }
   passCriteria.length = inputLength;
   return;
 }
-
 
 function isLower() {
   isLower = prompt("Do you want to have lower case letters? 'Y' for yes, 'N' for no");
@@ -77,7 +84,7 @@ function isSpecial() {
   while (isSpecial != "y" && isSpecial != "Y" && isSpecial != "N" && isSpecial != "n") {
     isSpecial = prompt("Incorrect response, please try again. Do you want to have special characters? 'Y' for yes, 'N' for no ");
   }
-  if (isUpper === "n" || isUpper === "N") {
+  if (isSpecial === "n" || isSpecial === "N") {
     passCriteria.special = false;
   }
   else {
@@ -86,29 +93,94 @@ function isSpecial() {
   return;
 }
 
+function isValid() {
+  while (passCriteria.lower === false && passCriteria.upper === false && passCriteria.numeric === false && passCriteria.special === false) {
+    prompt("Invalid, you must have some sort of characters, hit enter and try again")
+    inputLength()
+    isLower()
+    isUpper()
+    isNumeric()
+    isSpecial()
 
-getNumber()
+  }
+
+
+}
+
+
+function buildArray() {
+  if (passCriteria.lower === true) {
+    finalArray = finalArray.concat(lowerArray);
+  }
+  if (passCriteria.upper === true) {
+    finalArray = finalArray.concat(upperArray);
+  }
+  if (passCriteria.numeric === true) {
+    finalArray = finalArray.concat(numberArray);
+  }
+  if (passCriteria.special === true) {
+    finalArray = finalArray.concat(specialArray);
+  }
+}
+
+var endNumber = 0;
+function findEndNumber() {
+  endNumber = finalArray.length;
+  return endNumber;
+}
+
+
+
+function numberBetween0andMax(endNumber) {
+
+  return Math.floor(Math.random() * (endNumber));
+
+}
+
+
+
+
+
+
+inputLength()
 isLower()
 isUpper()
 isNumeric()
 isSpecial()
+isValid()
+buildArray()
+findEndNumber()
+numberBetween0andMax(endNumber)
+findCharacter()
 
-console.log(numberBetween0and9());
+function findCharacter() {
+  for (let i = 0; i < passCriteria.length; i++) {
+    const element = finalArray[(numberBetween0andMax(endNumber))];
+    passwordArray.push(element);
+  }
+}
+
+var finalPassword = passwordArray.join("")
+
+
+
 console.log(passCriteria.length);
 console.log(passCriteria.lower);
 console.log(passCriteria.upper)
 console.log(passCriteria.numeric)
 console.log(passCriteria.special)
+console.log(finalArray)
+console.log(finalArray.length)
+console.log(endNumber)
+console.log(numberBetween0andMax(endNumber))
+console.log(passwordArray)
+console.log(finalPassword)
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
-var randomNumber = 0;
-function numberBetween0and9() {
-
-  return Math.floor(Math.random() * 10);
-
-}
 
 
